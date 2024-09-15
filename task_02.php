22435d221acedc83c74d744c238f680db3ae5314
JahidHasan-00 <jahid.hasan50001@gmail.com>
2024-09-15 23:47:41 +0600

    created Filelogger class and have a public function logging

--- task_02.php
+++ task_02.php
@@ -6,8 +6,24 @@
     <title>Interface Implementation</title>
 </head>
 <body>
+<!-- Task 2: Interface Implementation**
+    Develop a logging system using interfaces. Create classes that implement a logging interface and demonstrate their usage. -->
+
     <?PHP 
-        echo"Interface Implementation";
+        interface loggingInterfaceSystem{
+            public function logging($userMessages);
+        }
+
+        class FileLogger implements loggingInterfaceSystem{
+            public function logging($userMessages){
+                file_put_contents("user.msg", $userMessages . "<br/>", FILE_APPEND);
+            }
+        }
+
+        $_File = new FileLogger();
+
+        $_File->logging("User is Successfully login");
+        $_File->logging("User is Successfully register");
     ?>
 </body>
 </html>
